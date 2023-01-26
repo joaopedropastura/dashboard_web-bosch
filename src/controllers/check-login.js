174f73336ff222f08ae0402c8b4241fe4a38aca4
jpsoares01 <jpsoares111@hotmail.com>
2023-01-26 16:44:16 -0300

    login onfire bebe

--- src/controllers/check-login.js
+++ src/controllers/check-login.js
@@ -1,18 +1,39 @@
 
 const aluno = require('../model/alunos')
 const instrutor = require('../model/instrutores')
+
 module.exports = {
     async checkLogin(req,res){
     
         const dados = req.body
+        var podeEntrar = false;
         console.log(dados)
         if (dados.edv && dados.senha){
-            query = `
-                SELECT * FROM Instrutores
-                WHERE EDV = "${dados.edv}"
-            `;
+            const alunos = await aluno.findAll({
+                raw: true,
+                attributes: ["EDV", "Nome", "Senha"]
+            })
+            const instrutores = await instrutor.findAll({
+                raw: true,
+                attributes: ["EDV", "Nome", "Senha"]
+            })
 
-            database
+            alunos.forEach(ele => {
+                if(dados.edv == ele.EDV && dados.senha == ele.Senha)
+                {
+                    podeEntrar = true
+
+                }
+            });
+            instrutores.forEach(ele => {
+                if(dados.edv == ele.EDV && dados.senha == ele.Senha)
+                {
+                    podeEntrar = true
+                    
+                }
+            });
+            if(podeEntrar)
+                res.redirect('/profile')
         }
         else{
             res.send('please enter email address and password details')
