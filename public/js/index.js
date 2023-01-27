11a4fbb48910c68e0a2921db4409f54cc3ff077f
Kaiky <kaikysr2004@gmail.com>
2023-01-27 09:31:08 -0300

    funcoes abrir cadastro

--- public/js/index.js
+++ public/js/index.js
@@ -1,8 +1,52 @@
+var turma = document.getElementById('cadastroTurma');
+var usuario = document.getElementById('cadastroUsuario');
+var disciplina = document.getElementById('cadastroDisciplina');
+var conteudo = document.getElementById('cadastroConteudo');
 
-function cardBody_text(item)
+const showTurma = () =>
 {
-    
-    let algo = document.getElementById('turma');
-    algo.style.display = flex;
-    
+  console.log(turma, usuario, disciplina, conteudo)
+  if(turma.classList.contains('show'))
+  {
+    turma.classList.replace('show', 'hidden');
+    usuario.classList.replace('hidden', 'show');
+    disciplina.classList.replace('hidden', 'show');
+    conteudo.classList.replace('hidden', 'show');
+  } 
+}
+
+const showUsuario = () => 
+{
+    console.log(turma, usuario, disciplina, conteudo)
+    if(usuario.classList.contains('show'))
+    {
+      turma.classList.replace('hidden', 'show');
+      usuario.classList.replace('show', 'hidden');
+      disciplina.classList.replace('hidden', 'show');
+      conteudo.classList.replace('hidden', 'show');
+    } 
+}
+
+  const showDisciplina = () => 
+{
+    console.log(turma, usuario, disciplina, conteudo)
+    if(turma.classList.contains('show'))
+    {
+      turma.classList.replace('hidden', 'show');
+      usuario.classList.replace('hidden', 'show');
+      disciplina.classList.replace('show', 'hidden');
+      conteudo.classList.replace('hidden', 'show');
+    } 
+}
+
+  const showConteudo = () => 
+{
+    console.log(turma, usuario, disciplina, conteudo)
+    if(turma.classList.contains('show'))
+    {
+      turma.classList.replace('hidden', 'show');
+      usuario.classList.replace('hidden', 'show');
+      disciplina.classList.replace('hidden', 'show');
+      conteudo.classList.replace('show', 'hidden');
+    } 
 }
