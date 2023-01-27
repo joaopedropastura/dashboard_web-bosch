113af9990c2381d91a7e364b1b509adf0d081091
Kaiky <kaikysr2004@gmail.com>
2023-01-27 16:50:08 -0300

    criando tela de avaliação

--- server.js
+++ server.js
@@ -14,5 +14,5 @@ app.set('view engine', 'ejs');
 app.use(routes);
 
 app.listen(3000, () => {
-	console.log('Acesse: http://localhost:3000/login')
+	console.log('Acesse: http://localhost:3000/avaliacao')
 });
