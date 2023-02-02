4485e956b420b020515d01082f13f41ac2bcfc95
Joao Pedro Soares <jpsoares111@hotmail.com>
2023-02-02 19:11:37 -0300

    adicionando o passado

--- public/js/dash-board.js
+++ public/js/dash-board.js
@@ -1,6 +1,6 @@
 
 function getDados(conteudos, conteudo_questao, questao, provas, turmas, disciplinas, alunos, instrutores)
-{	
+{
 	console.log(alunos)
 	// console.log(conteudos, conteudo_questao, questao, provas, turmas, disciplinas, alunos, instrutores)
 	const container = d3.select(".graphics")
@@ -8,12 +8,12 @@ function getDados(conteudos, conteudo_questao, questao, provas, turmas, discipli
 
 	const bars = container
 			.selectAll('.bar')
-			.data([alunos, questao])
+			.data([alunos, questao.where('EDV', alunos.EDV)])
 			.enter()
 			.append('div')
 			.classed('bar', true)
 			.style('width', '20px')
 			.style('height', data => (alunos.EDV / 10000) + 'px')
-	console.log(joao);
+	console.log(bars);
 
 }
